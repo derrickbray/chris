15e26a48ea7bebaa6ba18cb8331ad40142f6c88d
Derrick Bray <derrickbray@yahoo.com>
2017-12-17 14:46:20 -0600

    add icons and create the grey to blue

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function show(id, color) {
+function show(id, color, num) {
   var content = document.getElementsByClassName("right");
   var option = document.getElementsByClassName('options');
   var o;
@@ -14,4 +14,16 @@ function show(id, color) {
     content[c].style.display = "none";
   }
   var element = document.getElementById(id).style.display = 'block';
+
+  var greyIcons = document.getElementsByClassName('option-icon_grey');
+  var blueIcons = document.getElementsByClassName('option-icon_blue');
+  var i;
+  for (var i = 0; i < greyIcons.length; i++) {
+    greyIcons[i].style.display = 'block';
+    blueIcons[i].style.display = 'none';
+  }
+  var currentGreyIcon = document.getElementById('icon-grey' + num);
+  currentGreyIcon.style.display = 'none';
+  var currentBlueIcon = document.getElementById('icon-blue' + num);
+  currentBlueIcon.style.display = 'block';
 }
