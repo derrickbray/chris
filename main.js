19f0023f4f95f1cbed6e8b0014f4134a2162acd4
Derrick Bray <derrickbray@yahoo.com>
2017-12-17 10:54:36 -0600

    worked on styles

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ function show(id, color) {
   var option = document.getElementsByClassName('options');
   var o;
   for (o = 0; o < option.length; o++) {
-    option[o].style.background = "blue";
+    option[o].style.background = "linear-gradient(to right, #ebeced , #d1d5d6)";
+    option[o].style.color = "#9ba0a4";
   }
-  var currentSide = document.getElementById(color).style.background = 'black';
+  var currentSide = document.getElementById(color).style;
+  currentSide.background = 'white';
+  currentSide.color = '#36abe1';
   var c;
   for (c = 0; c < content.length; c++) {
     content[c].style.display = "none";
